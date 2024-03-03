5ab64a984a99a9a26dc3bf1933d074a022f15f27
matt <1654568495@qq.com>
2024-03-03 17:44:06 +0800

    asdf

--- matt-ichrak/vite.config.js
+++ matt-ichrak/vite.config.js
@@ -2,6 +2,8 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import { Plugin as importToCDN } from 'vite-plugin-cdn-import''
 
 // https://vitejs.dev/config/
 export default defineConfig({
