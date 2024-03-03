4708a509d1615dd65bed230929ab3c76a728a1af
matt <1654568495@qq.com>
2024-03-04 01:15:39 +0800

    asd

--- ichrak-matt/src/App.vue
+++ ichrak-matt/src/App.vue
@@ -2,7 +2,6 @@
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 </script>
-
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
